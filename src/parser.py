a3e4d4754d748942a6fc59b0a861874b4edef93f
PancakeBoy1987 <zane41181@gmail.com>
2025-11-21 23:53:39 +0300

    added new files made function for parcing data(city,nap)

--- src/parser.py
+++ src/parser.py
@@ -3,21 +3,11 @@ from bs4 import BeautifulSoup
 
 
 
-
-
-url = "https://tabiturient.ru"
-
-#page = requests.get(url, headers=headers)
-
-#soup = BeautifulSoup(page.text, "html.parser")
-
-
-
 url1 = "https://tabiturient.ru/ajax/ajcity.php"
 
 params = {
     "method": "getSpecList",
-    "region_id": "1",
+    "region_id": "2",
     "city_id": "1149",
 }
 
@@ -31,16 +21,6 @@ headers = {
 r = requests.get(url1, params=params, headers=headers)
 
 print("STATUS:", r.status_code)
-#print("TEXT:", r.text)  # показываем первые 500 символов
-
-#data = r.json()
-#print(data)
-
-#print(soup)
-
-##########
-#########
-#########
 
 
 soup = BeautifulSoup(r.text, "html.parser")
@@ -48,11 +28,80 @@ links = soup.find_all("a")
 
 
 links_list = []
+names_list = []
 
 for link in links:
     if 'href' in link.attrs:
         
         links_list.append(link['href'])
 
-print(links_list)
 
+
+for link in links:
+    city = link.find('span')
+    city = str(city)
+    names_list.append(city[23:-11])
+
+
+
+####ready list
+
+for i in range(len(links_list)):
+    print(names_list[i] + ' - ' + links_list[i])
+
+
+######
+######
+######
+######
+url = ("https://tabiturient.ru")
+
+
+def findNap():
+    global url
+    url_4 = url + "/ajax/ajnap.php"
+
+    params = {
+    "method": "getSpecList",
+    "region_id": "2",
+    "city_id": "1149",
+}
+
+    headers = {
+        "User-Agent": "Mozilla/5.0",
+        "Referer": "https://tabiturient.ru/",
+        "X-Requested-With": "XMLHttpRequest"
+    }
+    response = requests.get(url_4, params=params, headers=headers)
+    return response.text
+
+
+
+#### for finding naps in every vuz
+naps = findNap()
+
+
+sorted_naps = BeautifulSoup(naps,'html.parser')
+
+n_links = sorted_naps.find_all('a')
+
+
+nap_link_list = []
+for n_link in n_links:
+    if 'href' in n_link.attrs:
+        nap_link_list.append(n_link['href'])
+
+
+
+nap_name_list =[]
+
+for n_link in n_links:
+    f = n_link.find('b')
+    f = str(f)
+    nap_name_list.append(f[3:-4])
+
+
+####ready list
+
+for i in range(len(nap_link_list)):
+    print(nap_name_list[i] + ' - ' + nap_link_list[i])
