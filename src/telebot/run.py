f08e57014094110ca35bc5b2ecf951ea173159d5
QiriQqq <qiriq.q142@gmail.com>
2025-11-22 12:21:52 +0300

    Добавил пару файлов для будущего бота

---
+++ src/telebot/run.py
@@ -0,0 +1,20 @@
+import asyncio
+
+from handlers import router
+from aiogram import Bot, Dispatcher
+from config import TOKEN
+
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+async def main():
+    dp.include_router(router)
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
+
