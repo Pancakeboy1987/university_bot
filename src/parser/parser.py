bb39e425efdcf1507f3a00378fdefdb641b2002d
Akechi Goro <zane41181@gmail.com>
2025-12-02 18:51:18 +0300

    Теперь реализован поиск направлений в одном вузе и теперь выводится id и ссылочное имя вуза

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -148,7 +148,7 @@ def findUnisInCity(links_list):
 
 ###вызов функции присылает все вузы по городам
 
-findUnisInCity(links_list)
+#findUnisInCity(links_list)
 
 
 
@@ -193,4 +193,36 @@ def findNapsInCity(links_list):
         print("-" * 30)
 
 ###вывод всех направлений по городам
-findNapsInCity(links_list)
+#findNapsInCity(links_list)
+
+
+
+def findNapsInUni(vuz):
+    global url
+    url_4 = url + "/vuzu/"+vuz+'/about/'
+    params = {
+        "method": "getSpecList",
+        "region_id": "2",
+        "city_id": "1149",
+    }
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0 Safari/537.36",
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+        "X-Requested-With": "XMLHttpRequest",
+        "Referer": "https://tabiturient.ru/",
+    }
+
+    r = requests.get(url_4,  headers=headers)
+    soup = BeautifulSoup(r.text, "html.parser")
+    spec_body = soup.find('div', class_='p40 pm40')
+    specs = spec_body.find_all('a')
+    for spec in specs:
+        link = spec['href']
+        code = str(spec.find('span',class_='font11'))[21:44]
+        name = str(spec.find('b'))[3:-4]
+        print('Ссылка - ' + link + ',  - ' + code + ' - ' + name)
+        print()
+
+
+print(findNapsInUni('rudn'))
--- src/parser/unis.py
+++ src/parser/unis.py
@@ -25,7 +25,26 @@ def findUni(city):
     response = requests.post(url_5, data=data, headers=headers)
     return response.text
 
+### Здесь реализован запрос по id вуза, чтобы узнать его англ.название
 
+def findLink(id):
+    global url
+    url_6 = url + "/ajax/ajshowmoreinfof4.php"
+
+    data = {
+        'id': id,
+        'specid': ''
+    }
+    headers = {
+        "User-Agent": "Mozilla/5.0",
+        "Referer": "https://tabiturient.ru/",
+        "X-Requested-With": "XMLHttpRequest"
+    }
+    response = requests.post(url_6, data=data, headers=headers)
+    soap = BeautifulSoup(response.text, 'html.parser')
+    links = soap.find_all('a')
+    eng = str(links[0]['href'])[28:-6]
+    return eng
 
 
 
@@ -35,8 +54,10 @@ def findUnisInCity(links_list):
     for city in citiesList:
         soup = BeautifulSoup(findUni(city), 'html.parser')
         print(f'Универы города - {city}')
-        vuz_list = soup.find_all('table', class_='vuzlist p20')
+        vuz_list = soup.find_all('div', class_='mobpadd20')
         for vuz in vuz_list:
+            idVuz = str(vuz['id'])[3:]
+            eng_name = str(findLink(idVuz))
             short_name_tag = vuz.find('span', class_='font3').text
             long_name_tag = vuz.find('span', class_='font2').text
             if short_name_tag and long_name_tag:
@@ -44,7 +65,10 @@ def findUnisInCity(links_list):
                 full_name = long_name_tag.strip()
 
                 print(f"Кратко: {short_name}")
-                print(f"Полное: {full_name}\n\n")
+                print(f"Полное: {full_name}")
+                print(f"Название для ссылки - {eng_name}")
+                print(f"ID = {idVuz}\n\n")
+
         print("-" * 30)
 
 ###вызов функции присылает все вузы по городам
